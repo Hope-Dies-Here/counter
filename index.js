d002409059ac04629afd746900b7f88581546e0a
Dummy <onyourleftpal@gmail.com>
2023-04-26 00:09:48 +0300

    commit

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 const $ = (id) => document.querySelector(id)
 let number = 0
 $(".number").innerText = number
-
+let payment = 0
+/*
 $("#dec").addEventListener("click", () => {
   if(number > -5) number--
   else alert("buy pro for more clicks") 
   $(".number").innerText = number
 })
+*/
 
 $("#inc").addEventListener("click", () => {
-  if(number < 5) number++
-  else alert("buy pro for more clicks") 
+  if(number == 5) {
+    payment = payment + 10
+    number = 0
+    alert("rip that wallet man -$10 ")
+    $("#pay").innerText = `your bill is $${payment}`
+  }  
+  else number++
   $(".number").innerText = number
 })
 
