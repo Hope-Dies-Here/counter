c32d1b9292d32aea74b720d8ed4f57d324662632
Dummy <onyourleftpal@gmail.com>
2023-04-26 11:28:54 +0300

    setInterval

--- index.js
+++ index.js
@@ -1,29 +1,36 @@
-const $ = (id) => document.querySelector(id)
-let number = 0
-$(".number").innerText = number
-let payment = 0
-/*
-$("#dec").addEventListener("click", () => {
-  if(number > -5) number--
-  else alert("buy pro for more clicks") 
-  $(".number").innerText = number
-})
-*/
+const bill = document.getElementById("bill");
+const counter = document.getElementById("counter");
+const payBtn = document.getElementById("pay");
+const startBtn = document.getElementById("start");
 
-$("#inc").addEventListener("click", () => {
-  if(number == 5) {
-    payment = payment + 10
-    number = 0
-    alert("rip that wallet man -$10 ")
-    $("#pay").innerText = `your bill is $${payment}`
-  }  
-  else number++
-  $(".number").innerText = number
-})
+let number = 0;
+let payment = 0;
 
-$("#reset").addEventListener("click", () => {
-  number = 0
-  $(".number").innerText = number
-})
+startBtn.addEventListener("click", function () {
+  startBtn.setAttribute("disabled", true);
 
-console.log('counter will return!')
+  const interval = setInterval(function () {
+    number++;
+    if (number === 6) {
+      alert("that will be 10$");
+      number = 0;
+      payment = payment + 10;
+      clearInterval(interval);
+      startBtn.removeAttribute("disabled");
+    }
+    bill.innerText = `your bill is $${payment}`;
+    counter.innerText = number;
+  }, 1000);
+});
+
+payBtn.addEventListener("click", function () {
+  if (payment > 0) {
+    payment = 0;
+    alert("the government paid your bill");
+    bill.innerText = `your bill is $${payment}`;
+  } else {
+    alert("What are u paying ?");
+  }
+});
+
+console.log("counter will return!");
