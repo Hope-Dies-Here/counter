e15e89042ce48b6af6ee80dc484e0639cde0f2e9
Dummy <onyourleftpal@gmail.com>
2023-04-27 21:22:40 +0300

    array

--- js2/index.js
+++ js2/index.js
@@ -4,24 +4,11 @@ const input = document.getElementById("link")
 const ul = document.getElementById("display")
 
 const links = []
-const obj = [{
-  "name": "hoho",
-  "link": "hoho.com"
-},
-{
-  "name": "opeo",
-  "link": "opeo.com"
-}
-]
 
 submitBtn.addEventListener("click", function() {
   links.push(input.value)
   alert("link submited")
   input.value = ""
-  obj.forEach((o) => {
-    console.log(o.name)
-    
-  })
 })
 
 showBtn.addEventListener("click", function() {
@@ -29,8 +16,6 @@ showBtn.addEventListener("click", function() {
   links.forEach(function(link, index) {
     const li = document.createElement("li")
      li.innerText = link
-    // li.innerText = `${link} is in line ${index + 1}`
-    //li.innerHTML = `<a href =${link}> ${link}</a>`
     ul.appendChild(li)
   })
 })
