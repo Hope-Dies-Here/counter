09af95ea0b0321cf5336be07fb0900494b158c06
Dummy <onyourleftpal@gmail.com>
2023-05-18 21:05:07 +0300

    phone book added

--- js4/index.js
+++ js4/index.js
@@ -60,3 +60,32 @@ mode.addEventListener("click", () => {
   }
     
 })
+
+// Phone Book
+const name = document.querySelector("#name");
+const phone = document.querySelector("#phone");
+const saveBtn = document.querySelector("#save");
+const display = document.querySelector("#ul");
+
+let contacts = [];
+/* contacts = localStorage.getItem("contacts")
+ */
+saveBtn.addEventListener("click", () => {
+  if (name.value.trim() != "" && phone.value.trim() != "") {
+    contacts.push({
+      name: name.value,
+      phone: phone.value,
+    });
+    alert("contact saved");
+    contacts.forEach((contact) => {
+      name.value = "";
+      phone.value = "";
+      let data = `${contact.name} <br> <sub> ${contact.phone}`;
+      let li = document.createElement("li");
+      li.innerHTML = data;
+      display.append(li);
+    });
+  } else {
+    alert("save what...??");
+  }
+});
